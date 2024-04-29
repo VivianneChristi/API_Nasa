57edafe669c59111a827efbe2d401a7369c5f7b1
RaphaCardoso <154370014+RaphaCardoso@users.noreply.github.com>
2024-04-29 13:46:17 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -42,33 +42,88 @@ setInterval(fetchRandomNasaImages, 10000); // 10000 milissegundos = 10 segundos
 //------------------------------------------ Script da Earth ----------------------------------------------
 
 
+//------------------------------------------ Script da Earth ----------------------------------------------
+
 async function locaisTerra() {
-    let lon = 43.21;
-    let lat = 22.95;
-    let ano = 2018;
-    const response = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${ano}-01-01&&dim=0.10&api_key=9WBfmyWKPIgoa0zmt2sGuWaCEYap3aL2zbaY9DrF`)
-        .then(response => {
-            if (!response.ok) throw new Error('Network response was not ok');
-            return response.json();
-        })
-        .then(data => {
-            const imgElements = document.querySelectorAll('.container');
+    const locais = [
+        {
+            nome: "Cristo Redentor",
+            lat: -22.95,
+            lon: -43.21,
+            ano: [2016, 2018, 2020]
+        },
+        {
+            nome: "Grande Muralha da China",
+            lat: 40.43,
+            lon: 116.57,
+            ano: [2016, 2018, 2020]
+        },
+        {
+            nome: "Machu Picchu",
+            lat: -13.16,
+            lon: -72.54,
+            ano: [2016, 2018, 2020]
+        },
+        {
+            nome: "Coliseu de Roma",
+            lat: 41.89,
+            lon: 12.49,
+            ano: [2016, 2018, 2020]
+        },
+        {
+            nome: "Chichén Itzá",
+            lat: 20.68,
+            lon: -88.56,
+            ano: [2016, 2018, 2020]
+        },
+        {
+            nome: "Taj Mahal",
+            lat: 27.17,
+            lon: 78.04,
+            ano: [2016, 2018, 2020]
+        },
+        {
+            nome: "Petra",
+            lat: 30.32,
+            lon: 35.44,
+            ano: [2016, 2018, 2020]
+        }
+    ];
+
+    document.getElementById('search').addEventListener('click', handleLocationSelection);
+
+    async function handleLocationSelection() {
+        const selectedLocationIndex = document.getElementById('localNum').selectedIndex;
+        const selectedLocation = locais[selectedLocationIndex - 1]; // -1 para compensar o "Escolha um Local"
+
+        const selectedYearIndex = document.getElementById('yearNum').selectedIndex;
+        const year = locais[selectedLocationIndex - 1].ano[selectedYearIndex - 1];
+
+        const lon = locais[selectedLocationIndex - 1].lon;
+        const lat = locais[selectedLocationIndex - 1].lat;
+        const ano = year;
+
+        try {
+            const response = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${ano}-01-01&&dim=0.10&api_key=9WBfmyWKPIgoa0zmt2sGuWaCEYap3aL2zbaY9DrF`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            const imgContainer = document.querySelector('.container_Image_Earth');
             const image = data.url;
             console.log(image);
-
-            for (let i = 0; i < imgElements.length; i++) {
-                const randomIndex = randomIndexes[i];
-                const imageUrl = items[randomIndex].links[0].href;
-                imgElements[i].src = imageUrl;
-                imgElements[i].alt = items[randomIndex].data[0].title || 'Imagem da NASA';
-            }
-        })
-        .catch(error => console.error('Erro ao fazer a chamada da API da NASA:', error));
-
-    ;
+            const img = document.createElement('img');
+            img.src = image;
+            imgContainer.appendChild(img); // Adicionando a imagem ao elemento pai na página HTML
+        } catch (error) {
+            console.error('Erro ao fazer a chamada da API da NASA:', error);
+        }
+    }
 }
 
 locaisTerra();
+
+//------------------------------------------ FIM Script da Earth ----------------------------------------------
 
 //------------------------------------------ FIM Script da Earth ----------------------------------------------
 
